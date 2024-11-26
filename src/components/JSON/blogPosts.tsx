c63774d5f9b88c38464e97e8a68915308479e179
Md. Sadmanur Islam Shishir <109598070+shishir1290@users.noreply.github.com>
2024-11-26 09:27:39 +0600

    new blog

--- src/components/JSON/blogPosts.tsx
+++ src/components/JSON/blogPosts.tsx
@@ -6,4 +6,11 @@ export const blogPosts = [
       "This Next.js and Tailwind CSS project delivers a streamlined platform showcasing various aspects of university life at AIUB, combining engaging UI/UX elements with robust backend functionalities.",
     link: "/blog/aiub-campus-life-project",
   },
+  {
+    title: "Designing A City View: A C++ Graphics Project",
+    image: "/blogs/cityView.png",
+    description:
+      "This C++ graphics project demonstrates the creation of a dynamic city view, utilizing graphics libraries to construct a visually rich and interactive cityscape.",
+    link: "/blog/designing-a-city-view",
+  },
 ];
